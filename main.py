618d9b5856bcb20b87dc604209bfbb2cdd487bf8
Ignas Rasimas <ignas0904@gmail.com>
2024-03-23 18:20:52 +0000

    added sorting and dictionary to list conversion functions

--- main.py
+++ main.py
@@ -4,14 +4,26 @@ def main():
 
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
+   
+  
     
-    print(get_word_count(file_contents))
-    print(count_letters(file_contents))
+    counted_letters = dict_to_list(count_letters(file_contents))
+    counted_letters.sort(reverse=True, key=sort_on)
 
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{get_word_count(file_contents)} words found in the document")
+    print_list_report(counted_letters)
+ 
 
 def get_word_count(text):
     words = text.split()
     return len(words)
+
+
+def sort_on(dict):
+    return dict["num"]
+
+
 
 def count_letters(text):
     letter_dictionary = {}
@@ -22,6 +34,17 @@ def count_letters(text):
                 letter_dictionary[char] +=1
             else:
                 letter_dictionary[char] = 1
+
+
     return letter_dictionary
 
+def dict_to_list(dict):
+    new_list = []
+    for item in dict:
+        new_list.append({"char":item, "num":(dict[item])})
+    return new_list
+
+def print_list_report(list):
+    for i in range(0, len(list)):
+        print(f"The {list[i]['char']} character was found {list[i]['num']} times")
 main()
